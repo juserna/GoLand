dc8c1be7fbd7e21c6460f0a9fce3aee127ff71ab
Juan David Serna <juan.serna@mercadolibre.com.co>
2022-06-26 15:59:52 -0500

    actualizacion de files acerca de errores

---
+++ errores.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var FailedPaymentError = &paymentError{msg: "your cc is not available"}
+var InsFoundsError = &paymentError{msg: "your founds are 0 hahaha"}
+
+type paymentError struct {
+	msg string
+}
+
+func (e *paymentError) Error() string {
+	return e.msg
+}
+
+func main() {
+	_, err := stub(1)
+
+	/* if err != nil {
+		if errors.Is(err, FailedPaymentError) {
+			fmt.Println("Please, contact your bank")
+		}
+		if errors.Is(err, InsFoundsError) {
+			fmt.Println("You do not have any money")
+		}
+	}*/
+
+	customerPaymentError := fmt.Errorf("the CC is expired %w", err)
+
+	fmt.Println(errors.Unwrap(customerPaymentError))
+}
+
+func stub(i int) (int, error) {
+	if i == 1 {
+		return 1, InsFoundsError
+	}
+	return 0, FailedPaymentError
+}
