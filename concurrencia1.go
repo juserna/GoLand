be549c43d6e8c74f5349c025fb82ff7653795a82
Juan David Serna <juan.serna@mercadolibre.com.co>
2022-06-26 16:41:22 -0500

    mi primer gorrutine

--- concurrencia1.go
+++ concurrencia1.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
 
+	for i := 0; i < 10; i++ {
+		fmt.Printf("number: %d \n", i)
+	}
+
+	fmt.Println("----------------")
+	i := 0
+	for ; i < 10; i++ {
+		go ping(i)
+	}
+	time.Sleep(2 * time.Second)
+
+}
+
+func ping(i int) {
+	fmt.Println(fmt.Sprintf("number: %d", i))
 }
